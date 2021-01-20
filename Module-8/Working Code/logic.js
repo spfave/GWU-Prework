385dd372a13c74d85d55b88a8239108e64ff5660
Sebastian F <sebpfave2@gmail.com>
2021-01-20 18:27:47 -0500

    Module-8 complete

--- Module-8/Working Code/logic.js
+++ Module-8/Working Code/logic.js
@@ -1,5 +1,5 @@
 // When the factButton is clicked...
-$("#factButton").on("click", function() {
+$("#factButton").on("click", function () {
 	// We generate a random number between 0 and 4 (the number of facts in the catFactArray)
 	var number = Math.floor((Math.random() * catFactArray.length));
 	// We display the fact from the catFactArray that is in the random position we just generated.
@@ -10,32 +10,32 @@ $("#factButton").on("click", function() {
 var catFactArray = ["Grumpy was born on April 4, 2012", "Grumpy's unique look comes from feline dwarfism and an underbite", "Grumpy has a brother named Pokey", "Her favorite Friskies food is Savory Shreds"]
 
 // When the textPink button is pressed...
-$("#textPink").on("click", function() {
+$("#textPink").on("click", function () {
 	// Change funText to pink.
 	$("#funText").css("color", "pink")
 })
 
 // When the textOrange button is pressed...
-$("#textOrange").on("click", function() {
+$("#textOrange").on("click", function () {
 	// Change funText to orange.
 	$("#funText").css("color", "orange")
 })
 
 // When the textGreen button is pressed...
-$("#textGreen").on("click", function() {
+$("#textGreen").on("click", function () {
 	// Change funText to green.
 	$("#funText").css("color", "green")
 })
 
 
 // When the boxGrow button is clicked... 
-$("#boxGrow").on("click", function() {
+$("#boxGrow").on("click", function () {
 	// Increase the size of the box.
-	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
+	$("#box").animate({ height: "+=35px", width: "+=35px" }, "fast");
 })
 
 // When the boxShrink button is clicked...
-$("#boxShrink").on("click", function() {
+$("#boxShrink").on("click", function () {
 	// Decrease the size of the box.
-	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
+	$("#box").animate({ height: "-=35px", width: "-=35px" }, "fast");
 })
